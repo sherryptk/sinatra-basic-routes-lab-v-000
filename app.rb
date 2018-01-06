425950c17c35de18fa95bb1d3e584bb6c9cac5df
Sherry El Garhy <sherryptk@gmail.com>
2018-01-06 22:40:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Sherene."
+  end
+
+  get '/hometown' do
+    "My hometown is Pontiac."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Another Day of Sun."
+  end
 end
